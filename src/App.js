6d6b107681917aefeaeae14687449c3f10b44b74
unknown <aman.ap.23052001@gmail.com>
2024-01-04 22:42:48 +0530

    switch is not working in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from "react";
-import { Switch , Route , Link } from "react-router-dom";
+import "./App.css"
+import { Switch, Route, Link } from 'react-router-dom';
 import { Layout , Typography , Space} from "antd";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 
 function App() {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
