ebc2e9a7f9bb49b21c1b95be69567d0dca092470
unknown <aman.ap.23052001@gmail.com>
2024-01-05 22:34:45 +0530

    error in fetching

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,12 @@ const {Title } = Typography;
 
 const Homepage = () => {
   const {data , isFetching} = useGetCryptosQuery();
-  console.log(data)
+
+  const globalStats = data?.data?.stats ;
+  
+  if(isFetching){
+    return 'Loading...'
+  }
 
   return (
     <div>
@@ -17,7 +22,7 @@ const Homepage = () => {
       </Title>
 
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={5} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={5} /></Col>
         <Col span={12}><Statistic title="Total Market Cap" value={5} /></Col>
         <Col span={12}><Statistic title="Total 24h Volume" value={5} /></Col>
