1dadb5f11ee904383d6d2f9d43d44189a142d72e
unknown <aman.ap.23052001@gmail.com>
2024-01-06 16:31:16 +0530

    not getting cryptonews in console instead of this its is showing undefined

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,7 +9,7 @@ import  News  from "../components/News";
 const {Title } = Typography;
 
 const Homepage = () => {
-  const {data , isFetching , isError} = useGetCryptosQuery();
+  const {data , isFetching , isError} = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats ;
   
   if(isFetching){
@@ -37,12 +37,12 @@ const Homepage = () => {
         <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
         <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show More</Link></Title>
       </div>
-      <Cryptocurrencies />
+      <Cryptocurrencies simplified/>
       <div className='home-heading-container'>
         <Title level={2} className='home-title'>Latest Crypto News</Title>
         <Title level={3} className='show-more'><Link to="/news">Show More</Link></Title>
       </div>
-      <News />
+      <News simplified/>
     </>
   )
 }
